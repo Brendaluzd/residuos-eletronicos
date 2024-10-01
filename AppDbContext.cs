076784984cb96e376a8860a7d6f771193fed5c00
Brendaluzd <157062789+Brendaluzd@users.noreply.github.com>
2024-10-01 15:34:33 -0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResiduosEletronicos.Models;
+
+namespace ResiduosEletronicos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PontoDeColeta> PontosDeColeta { get; set; }
+    }
+}
---
+++ PontoDeColeta.cs
@@ -0,0 +1,12 @@
+﻿namespace ResiduosEletronicos.Models
+{
+    public class PontoDeColeta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Telefone { get; set; }
+    }
+}
